90bd458eaff3f801a16bcfffe35b2ee0d1e206e5
moe-ped <m.berning@web.de>
2015-07-28 17:23:47 +0200

    Added vertical solving and clearing

--- Assets/Tribble.cs
+++ Assets/Tribble.cs
@@ -15,38 +15,86 @@ public class Tribble : MonoBehaviour {
 							 "tree",
 							 "old"
 							 };*/
+	// TODO: see if there is a better container for this xD
+	private List<KeyValuePair<KeyValuePair<int, int>, KeyValuePair<int, int>>> WordsToClear = new List<KeyValuePair<KeyValuePair<int,int>,KeyValuePair<int,int>>>();
 
 	// Use this for initialization
 	void Start () {
-		// Just in case it causes issues
-		//InitializeOutputLetters();
 		ReadOutputText();
+
 		// Test
 		Words.Add("dog");
 		Words.Add("tree");
 		Words.Add("old");
-		//SetLetter(3, 0, "t".ToCharArray()[0]);
-		Solve(3, 0);
+
+		SetLetter(3, 0, "t");
+		//Solve(3, 0);
+		//ClearWords();
 		WriteOutputText();
 	}
 
 	void Solve(int changedX, int changedY) {
+		// Horizontal
 		int startLetter = 0;
 		string line = lines[changedY];
-		int endLetter = line.Length;
-		while (startLetter != endLetter + 1) {
-			int length = endLetter - startLetter;
-			if (length < 3 || endLetter == changedX) {
+		int endLetter = line.Length - 1;
+		string word;
+
+		while (startLetter <= endLetter) {
+			int length = endLetter - startLetter + 1;
+			if (length < 3 || endLetter < changedX) {
 				startLetter++;
-				endLetter = line.Length;
+				endLetter = line.Length - 1;
 				continue;
 			}
-			string word = line.Substring(startLetter, length);
+			word = line.Substring(startLetter, length);
 			if (Words.Contains(word)) {
-				Debug.Log(word);
+				KeyValuePair<int, int> start = new KeyValuePair<int,int> (startLetter, changedY);
+				KeyValuePair<int, int> end = new KeyValuePair<int,int> (endLetter, changedY);
+				KeyValuePair<KeyValuePair<int, int>, KeyValuePair<int, int>> wordToClear = new KeyValuePair<KeyValuePair<int,int>, KeyValuePair<int,int>> (start, end);
+				WordsToClear.Add(wordToClear);
 			}
 			endLetter--;
 		}
+
+		// Vertical
+		startLetter = 0;
+		endLetter = lines.Length - 1;
+		word = "";
+
+		while (startLetter <= endLetter) {
+			int length = endLetter - startLetter + 1;
+			if (length < 3 || endLetter < changedY) {
+				startLetter++;
+				endLetter = lines.Length - 1;
+				continue;
+			}
+			// Assemble word
+			for (int i = startLetter; i <= endLetter; i++) {
+				if (changedX > lines[i].Length) break;
+				word += lines[i][changedX];
+			}
+			if (Words.Contains(word)) {
+				KeyValuePair<int, int> start = new KeyValuePair<int,int> (changedX, startLetter);
+				KeyValuePair<int, int> end = new KeyValuePair<int,int> (changedX, endLetter);
+				KeyValuePair<KeyValuePair<int, int>, KeyValuePair<int, int>> wordToClear = new KeyValuePair<KeyValuePair<int,int>, KeyValuePair<int,int>> (start, end);
+				WordsToClear.Add(wordToClear);
+			}
+			endLetter--;
+		}
+	}
+
+	void ClearWords() {
+		foreach (var word in WordsToClear) {
+			for (int y = word.Key.Value; y <= word.Value.Value; y++) {
+				for (int x = word.Key.Key; x <= word.Value.Key; x++) {
+					var arr = lines[y].ToCharArray();
+					arr[x] = ' ';
+					lines[y] = new string(arr);
+				}
+			}
+		}
+		WordsToClear = new List<KeyValuePair<KeyValuePair<int, int>, KeyValuePair<int, int>>>();
 	}
 
 	void ReadOutputText() {
@@ -63,8 +111,8 @@ public class Tribble : MonoBehaviour {
 		OutputText.text = text;
 	}
 
-	void SetLetter(int x, int y, char letter) {
-		lines[y].Insert(x, letter.ToString());
+	void SetLetter(int x, int y, string letter) {
+		lines[y].Insert(x, letter);
 		WriteOutputText();
 	}
 }
